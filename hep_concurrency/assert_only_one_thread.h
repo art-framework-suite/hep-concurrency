3c4a2c6fe41198f28b127aa5f51263ca6088b88c
Zachary Evans <ztevans@fnal.gov>
2023-08-18 14:09:49 -0500

    Format code.

--- hep_concurrency/assert_only_one_thread.h
+++ hep_concurrency/assert_only_one_thread.h
@@ -30,8 +30,11 @@ namespace hep::concurrency::detail {
 
   class thread_counter {
   public:
-    explicit thread_counter(std::source_location loc = std::source_location::current())
-      : filename_{loc.file_name()}, linenum_{loc.line()}, funcname_{loc.function_name()}
+    explicit thread_counter(
+      std::source_location loc = std::source_location::current())
+      : filename_{loc.file_name()}
+      , linenum_{loc.line()}
+      , funcname_{loc.function_name()}
     {}
 
     class sentry {
@@ -67,7 +70,7 @@ namespace hep::concurrency::detail {
 #define HEP_CONCURRENCY_ASSERT_ONLY_ONE_THREAD()
 #else // NDEBUG
 #define HEP_CONCURRENCY_ASSERT_ONLY_ONE_THREAD()                               \
-  static hep::concurrency::detail::thread_counter s_tc_{};                          \
+  static hep::concurrency::detail::thread_counter s_tc_{};                     \
   hep::concurrency::detail::thread_counter::sentry sentry_tc_                  \
   {                                                                            \
     s_tc_                                                                      \
