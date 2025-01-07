4167d24479f7648207b4aa269d2c08c6627342b0
Chris Green <greenc@fnal.gov>
2025-01-07 13:04:00 -0600

    Remove obsolete handling of library variables

--- config/hep_concurrency-augment-HEP_CONCURRENCY.cmake
+++
@@ -1,9 +0,0 @@
-if (HEP_CONCURRENCY)
-  list(PREPEND HEP_CONCURRENCY
-    hep_concurrency::thread_sanitize
-  )
-  list(APPEND HEP_CONCURRENCY
-    hep_concurrency::cache
-    hep_concurrency::simultaneous_function_spawner
- )
-endif()
